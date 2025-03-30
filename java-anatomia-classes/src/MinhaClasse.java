0d33c501640574170e939297bc12946cfdc394fd
F-Henrique <santos.rique89@gmail.com>
2025-03-29 21:07:48 -0300

    Subindo primeiro programa em java

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,22 @@
+
+public class MinhaClasse {
+    
+    public static void main(String[] args) {
+        
+        String primeiroNome ="henrique";
+        String segundoNome = "santos";
+        // System.out.print(primeiroNome & " " & segundoNome);
+
+        String nomeCompleto =NomeCompleto(primeiroNome, segundoNome);
+        System.out.print(nomeCompleto);
+        
+    }
+
+public static String NomeCompleto(String primeiroNome, String segundoNome){
+    // return primeiroNome.concat(" ").concat(segundoNome);
+    return primeiroNome + " "+segundoNome;
+}
+
+
+
+}
