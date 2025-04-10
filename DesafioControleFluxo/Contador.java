3277f9eff7bb049f7c631d32d9019ec263b07b6d
F-Henrique <santos.rique89@gmail.com>
2025-04-10 00:02:52 -0300

    desafio controle de fluxo

--- DesafioControleFluxo/Contador.java
+++ DesafioControleFluxo/Contador.java
@@ -4,6 +4,7 @@ public class Contador {
 	public static void main(String[] args) {
 
 		Scanner terminal = new Scanner(System.in);
+		
 		System.out.println("Digite o primeiro parâmetro");
 		int parametroUm = terminal.nextInt();
 		System.out.println("Digite o segundo parâmetro");
